ea70082e8030befadd888ca83a03f864e56f3a98
EuronRamadani <euronra@gmail.com>
2026-01-05 18:32:54 +0100

    Implement the whole Departament feature in the system

---
+++ RMP.Core.Host/Entities/DepartmentProfessorEntity.cs
@@ -0,0 +1,12 @@
+namespace RMP.Host.Entities;
+
+public class DepartmentProfessorEntity
+{
+   
+    public Guid DepartmentId { get; set; }
+    public Guid ProfessorId { get; set; }
+    public virtual ProfessorEntity Professor { get; set; }
+    public DepartmentEntity Department { get; set; }
+   
+    
+}
